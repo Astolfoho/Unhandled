ec51daec3cccfebcb213e331f9b9596727e942d1
Astolfo Hoscher <astolfo@outlook.com.br>
2015-09-22 18:02:14 -0300

    added unity to api

---
+++ Main/UnhandledApi/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+using Microsoft.Practices.Unity;
+using UnhandledApi.Repositories.Interfaces;
+
+[assembly: OwinStartup(typeof(UnhandledApi.Startup))]
+
+namespace UnhandledApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {      
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var container = new UnityContainer();
+
+            container.RegisterTypes(AllClasses.FromLoadedAssemblies(),
+                                    WithMappings.FromMatchingInterface,
+                                    WithName.Default,
+                                    WithLifetime.ContainerControlled);
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+    
+        }
+    }
+}
---
+++ Main/UnhandledApi/UnityDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace UnhandledApi
+{
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+        private UnityContainer _container;
+
+        public UnityDependencyResolver(UnityContainer container)
+        {
+            this._container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _container.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.ResolveAll(serviceType);
+        }
+
+    }
+}
